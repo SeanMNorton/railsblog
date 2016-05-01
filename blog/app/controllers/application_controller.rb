baa5398fbfc0392b5b7a0290b75c5898ef8ab322
Sean Norton <NortonSeanM@gmail.com>
2016-05-01 17:40:54 -0500

    Added some features

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  # rescue_from ActionController::RoutingError, :with => :not_found
   protect_from_forgery with: :exception
+
+
+  def not_found
+    redirect_to root_path
+  end
 end
