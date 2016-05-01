c444dedaf95878bcdcf23dc4754a300981d16a3c
Sean Norton <NortonSeanM@gmail.com>
2016-05-01 16:05:42 -0500

    add delete comments

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  # if post gets deleted comments too get smashed into nothing with dependent
+  has_many :comments, dependent: :destroy
   #Classic validations with title min 5chars
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Resources sets up gets for all of posts paths
-  resources :posts
+  # Resources sets up gets for all of posts paths nests comments on posts
+  resources :posts do
+    resources :comments
+  end
   # Sets home/root or '/' path to '/posts'
   root "posts#index"
 end
